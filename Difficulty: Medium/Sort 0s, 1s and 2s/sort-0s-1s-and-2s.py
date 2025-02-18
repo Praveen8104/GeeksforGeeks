e47d2dc95e414d27a7614a5337a40676dc2ee96d
Praveen Reddy Goli <163567436+Praveen8104@users.noreply.github.com>
2025-02-18 11:45:06 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Sort 0s, 1s and 2s/sort-0s-1s-and-2s.py
@@ -0,0 +1,35 @@
+#{ 
+ # Driver Code Starts
+
+# } Driver Code Ends
+#User function Template for python3
+
+class Solution:
+    def sort012(self,arr):
+        # code here
+        arr=arr.sort()
+
+
+
+
+
+#{ 
+ # Driver Code Starts.
+def main():
+    t = int(input().strip())  # Read the number of test cases
+    ob = Solution()
+
+    while t > 0:
+        t -= 1
+        arr = list(map(int,
+                       input().strip().split())
+                   )  # Read the array as space-separated integers
+        ob.sort012(arr)  # Sort the array
+
+        print(' '.join(map(str, arr)))  # Print the sorted array
+        print("~")
+        
+if __name__ == "__main__":
+    main()
+
+# } Driver Code Ends
