42d7b8fc29083addf72f7e37f17f4fef409148c0
Praveen Reddy Goli <163567436+Praveen8104@users.noreply.github.com>
2025-03-11 22:36:02 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Binary Array Sorting/binary-array-sorting.py
@@ -0,0 +1,22 @@
+
+class Solution:
+    # Function to sort the binary array in non-decreasing order
+    def binSort(self, arr):
+        # code here
+        arr = arr.sort()
+        return arr
+
+
+
+#{ 
+ # Driver Code Starts
+# Driver code
+t = int(input())  # number of test cases
+for _ in range(t):
+    arr = list(map(int, input().split()))  # input array
+    Solution().binSort(arr)  # sort the binary array
+
+    print(*arr)  # print the sorted array
+    print("~")
+
+# } Driver Code Ends
